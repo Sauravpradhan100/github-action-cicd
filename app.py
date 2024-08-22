4b5ae293b2c965ead6d6eb890bb43f35b352ba79
Sauravpradhan100 <sauravpradhan.100@gmail.com>
2024-08-23 00:14:49 +0530

    all added

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+
+data = {
+            'Course Name': ['MySql', 'Linux', 'Hadoop', 'Python', 'DevOps', 'Azure', 'Pyspark'], 
+            'Duration': ['30 days', '15 days', '20 days', '35 days', '35 days', '25 days', '20 days'],
+            'Fees': ['8000', '5000', '10000', '10000', '20000', '10000', '7000']
+    }
+df = pd.DataFrame(data)
+
+st.dataframe(df)
+st.write('Total fees = 70000 Rupees')
