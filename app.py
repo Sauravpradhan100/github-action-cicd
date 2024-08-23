92a416f3d0b189ec4e9b6cea8b15b64d20c85c12
Sauravpradhan100 <sauravpradhan.100@gmail.com>
2024-08-23 19:38:49 +0530

    all added

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 
+st.write('Welcome To AVD Group')
+st.write('Below Fee Structure for Data Engineering')
+
 data = {
             'Course Name': ['MySql', 'Linux', 'Hadoop', 'Python', 'DevOps', 'Azure', 'Pyspark'], 
             'Duration': ['30 days', '15 days', '20 days', '35 days', '35 days', '25 days', '20 days'],
